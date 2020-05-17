791dbb1fccce9637030bd743e33b7655e5dbfd89
Angel Hernandez <angelh2m@icloud.com>
2020-05-17 15:19:55 -0500

    Changes

--- linear_scale.js
+++ linear_scale.js
@@ -2,8 +2,9 @@
 /* *
 *  LESSON 24 === https://github.com/iamshaunjp/data-ui-with-d3-firebase/blob/lesson-24/index.js
 * FINAL CODE https://github.com/iamshaunjp/data-ui-with-d3-firebase/blob/lesson-28/index.js
+* UPDATE FUNC CODE  https://github.com/iamshaunjp/data-ui-with-d3-firebase/blob/lesson-36/index.js
 */
-
+// select the svg container first
 const svg = d3.select('.canvas')
     .append('svg')
     .attr('width', 600)
@@ -14,62 +15,55 @@ const margin = { top: 20, right: 20, bottom: 100, left: 100 };
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-let render = async () => {
-    // let data = await d3.json("./menu.json");
-    let data = []
-    let firebaseData = await db.collection('dishes').get();
-    firebaseData.forEach(element => {
-        data.push(element.data())
-    });
-    console.warn(data);
+// create axes groups
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`)
 
-    const min = d3.min(data, d => d.orders);
-    const max = d3.max(data, d => d.orders);
-    const extent = d3.extent(data, d => d.orders);
-    console.log(min, max, extent);
+xAxisGroup.selectAll('text')
+    .attr('fill', 'orange')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end');
 
-    const graph = svg.append('g')
-        .attr('width', graphWidth)
-        .attr('height', graphHeight)
-        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+const yAxisGroup = graph.append('g');
 
+const y = d3.scaleLinear()
+    .range([graphHeight, 0]);
 
-    const xAxisGroup = graph.append('g')
-        .attr('transform', `translate(0, ${graphHeight})`)
-    const yAxisGroup = graph.append('g');
+const x = d3.scaleBand()
+    .range([0, graphWidth])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
-    /* *
-    *  This is for the bars
-    */
-    const y = d3.scaleLinear()
-        .domain([0, max])
-        .range([graphHeight, 0]);
+// create & call axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
 
-    /* *
-    *  This is to tell how wide the width needs to be
-    */
-    const x = d3.scaleBand()
-        .domain(data.map(item => item.name))
-        .range([0, 500])
-        .paddingInner(0.2)
-        .paddingOuter(0.2)
+// the update function
+const update = (data) => {
 
+    // join the data 
     const rects = graph.selectAll('rect')
         .data(data);
 
-    /* *
-    *  Add the first rectangle
-    */
-    // rects.attr('width', x.bandwidth)
-    //     .attr('height', d => graphHeight - y(d.orders))
-    //     .attr('fill', 'orange')
-    //     .attr('x', (d) => x(d.name))
-    //     .attr('y', d => y(d.orders));
+    // update the domains
+    y.domain([0, d3.max(data, d => d.orders)]);
+    x.domain(data.map(item => item.name));
 
-    /* *
-    *  Add the rest of rectangles
-    */
+    // add attrs to rects already in the DOM
+    rects.attr('width', x.bandwidth)
+        .attr("height", d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
+
+    // append the enter selection to the DOM
     rects.enter()
         .append('rect')
         .attr('width', x.bandwidth)
@@ -78,22 +72,16 @@ let render = async () => {
         .attr('x', (d) => x(d.name))
         .attr('y', d => y(d.orders));
 
-    // create & call axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y).ticks(6)
-
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis);
+};
 
-    xAxisGroup.selectAll('text')
-        // .attr('fill', 'orange')
-        .attr('transform', 'rotate(-40)')
-        .attr('text-anchor', 'end')
+db.collection('dishes').get().then(res => {
 
-}
+    var data = [];
+    res.docs.forEach(doc => {
+        data.push(doc.data());
+    });
 
-
-render();
-
-
-// console.log("LOADED");
+    update(data);
+});
